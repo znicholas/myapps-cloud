1fcccf2a02d919da16766b753884b8323005f60c
znicholas <zhen_002@qq.com>
2018-08-06 00:27:15 +0800

    add myapps-cloud

---
+++ myapps-eureka-server/src/main/java/cn/myapps/myappseurekaserver/MyappsEurekaServerApplication.java
@@ -0,0 +1,12 @@
+package cn.myapps.myappseurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyappsEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyappsEurekaServerApplication.class, args);
+	}
+}
