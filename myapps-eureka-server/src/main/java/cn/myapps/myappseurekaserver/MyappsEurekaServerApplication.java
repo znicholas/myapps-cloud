8538b07174127ce4dd1a6f4a9f0d554957f3f4e8
znicholas <zhen_002@qq.com>
2018-08-21 22:19:55 +0800

    add config server

--- myapps-eureka-server/src/main/java/cn/myapps/myappseurekaserver/MyappsEurekaServerApplication.java
+++ myapps-eureka-server/src/main/java/cn/myapps/myappseurekaserver/MyappsEurekaServerApplication.java
@@ -2,9 +2,11 @@ package cn.myapps.myappseurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableConfigServer
 @EnableEurekaServer
 public class MyappsEurekaServerApplication {
 
